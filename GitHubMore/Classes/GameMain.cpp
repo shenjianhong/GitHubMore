682d1d6b642d7877fb7b1e72efb9440c9ad83c5e
shenjianhong <87421834@qq.com>
2012-08-09 22:30:48 +0800

    1

--- GitHubMore/Classes/GameMain.cpp
+++ GitHubMore/Classes/GameMain.cpp
@@ -51,22 +51,77 @@ bool GameMain::init()
     {
         for (int y=0; y<9; y++)
         {
-            Grids[x][y].number = 0;
+            Grids[x][y].number = -1;
             Grids[x][y].testnumber = 0;
             Grids[x][y].x = x+1;
             Grids[x][y].y = y+1;
         }
     }
     
-    FILE* pf = fopen("test.txt", "rt");
+    const char* pathKey = CCFileUtils::fullPathFromRelativePath("test.txt");
+    FILE* pf = fopen(pathKey, "rt");
     if (pf!=NULL)
     {
-        char ch[10];
+        char ch;
         
-        fgets(ch, 10, pf);
+        ch = fgetc(pf);
+        int currentx = 0;
+        int currenty = 0;
+        
+        int num = 1;
+        CCLog("Test ------- %d -------", num);
+        while (ch != EOF)
+        {
+            if (ch == '\t' || (ch >= '0'&&ch <='9'))
+            {
+                
+                if (ch != '\t' )
+                {
+                    Grids[currentx][currenty].number = atoi(&ch);  
+                    CCLog("%d, %d, %d",currentx,currenty, Grids[currentx][currenty].number);
+                    currentx--;
+                }
+                else
+                {
+                    if(Grids[currentx][currenty].number == -1)
+                    {
+                        Grids[currentx][currenty].number = 0;  
+                    }
+                }
+                
+                ch = fgetc(pf);
+                currentx++;
+            }
+            else
+            {
+                currentx = 0;
+                currenty++;
+                if (currenty == 10)
+                {
+                    currenty = 0;
+                    
+                    for (int x=0; x<9; x++) 
+                    {
+                        for (int y=0; y<9; y++)
+                        {
+                            Grids[x][y].number = -1;
+                            Grids[x][y].testnumber = 0;
+                            Grids[x][y].x = x+1;
+                            Grids[x][y].y = y+1;
+                        }
+                    }
+                    num ++;
+                    CCLog("Test ------- end ------", num);
+                    CCLog("Test ------- %d -------", num);
+                }
+                ch = fgetc(pf);
+            }
+        }
+        CCLog("Test ------- end ------", num);
+       
         
     }
-    delete pf;
+    fclose(pf);
     
     
     return true;
